c72cd38cdfb0fe0206531999bffbbc8ae54c547a
Reinaldy Rafli <aldy505@proton.me>
2023-04-07 21:22:15 +0700

    Update business/payment_service/get_details.go
    Co-authored-by: yuu <72334350+YogiPristiawan@users.noreply.github.com>

--- business/payment_service/get_details.go
+++ business/payment_service/get_details.go
@@ -17,19 +17,19 @@ func (d *Dependency) GetDetails(ctx context.Context, id string) (business.Paymen
 	// Try virtual account
 	entry, err = d.virtualAccountRepository.Get(ctx, id)
 	if err != nil {
-		return business.PaymentDetailsResponse{}, fmt.Errorf("acquiring from virtual account store: %w", err)
-	}
-
-	// Try e-money
-	if err == nil && errors.Is(err, repository.ErrNotFound) {
-		entry, err = d.eMoneyRepository.Get(ctx, id)
-		if err != nil {
-			if errors.Is(err, repository.ErrNotFound) || errors.Is(err, repository.ErrExpired) {
-				return business.PaymentDetailsResponse{}, business.ErrTransactionNotFound
-			}
-
-			return business.PaymentDetailsResponse{}, fmt.Errorf("acquiring from emoney store: %w", err)
-		}
+	    if !errors.Is(err, repository.ErrNotFound) {
+	    		return business.PaymentDetailsResponse{}, fmt.Errorf("acquiring from virtual account store: %w", err)
+	    }
+	    
+	    // Try e-money
+        entry, err = d.eMoneyRepository.Get(ctx, id)
+        if err != nil {
+	        if errors.Is(err, repository.ErrNotFound) || errors.Is(err, repository.ErrExpired) {
+		        return business.PaymentDetailsResponse{}, business.ErrTransactionNotFound
+	        }
+        
+	        return business.PaymentDetailsResponse{}, fmt.Errorf("acquiring from emoney store: %w", err)
+        }
 	}
 
 	// Acquire more data from transaction repository
