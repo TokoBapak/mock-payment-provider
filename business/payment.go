4d83455c16d49de11e4e81df11644c1bf515b793
Reinaldy Rafli <aldy505@proton.me>
2023-04-14 18:59:07 +0700

    Update business/payment.go
    Co-authored-by: Cakrawala <48281037+WahidinAji@users.noreply.github.com>

--- business/payment.go
+++ business/payment.go
@@ -11,7 +11,7 @@ import (
 type Payment interface {
 	// GetDetails will acquire a payment detail from an ID coming from e-money or
 	// virtual account payment.
-	GetDetails(ctx context.Context, id string) (PaymentDetailsResponse, error)
+	GetDetail(ctx context.Context, id string) (PaymentDetailsResponse, error)
 	// MarkAsPaid will mark an order ID as paid. This one function must only be called
 	// from the presentation that handles payment confirmation from the customer's
 	// standpoint.
