99078433438d305c511fc3b842ce798ca3b9a123
WahidinAji <a17wahidin@gmail.com>
2023-06-12 14:43:14 +0700

    Finally, I've updated this PR hahaha

--- business/payment_service/get_details_test.go
+++ business/payment_service/get_details_test.go
@@ -2,191 +2,246 @@ package payment_service_test
 
 import (
 	"context"
-	"fmt"
+	"database/sql"
+	"errors"
+	"mock-payment-provider/business"
 	"mock-payment-provider/business/payment_service"
 	"mock-payment-provider/primitive"
 	"mock-payment-provider/repository"
+	"mock-payment-provider/repository/emoney"
+	"mock-payment-provider/repository/transaction"
+	"mock-payment-provider/repository/virtual_account"
+	"mock-payment-provider/repository/webhook"
+	"os"
 	"testing"
 	"time"
+
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type MockTransactionRepository struct{}
+var (
+	db  *sql.DB
+	cfg config
+)
 
-func (m *MockTransactionRepository) Migrate(ctx context.Context) error {
-	return nil
-}
-func (m *MockTransactionRepository) Create(ctx context.Context, params repository.CreateTransactionParam) error {
-	return nil
-}
-func (m *MockTransactionRepository) UpdateStatus(ctx context.Context, orderId string, status primitive.TransactionStatus) error {
-	return nil
-}
-func (m *MockTransactionRepository) GetByOrderId(ctx context.Context, orderId string) (primitive.Transaction, error) {
-	if orderId != "order-id" {
-		return primitive.Transaction{}, repository.ErrNotFound
+func TestMain(m *testing.M) {
+	var err error = nil
+	// db, err = sql.Open("sqlite3", ":memory:?_txlock=exclusive&_foreign_keys=1&")
+	// if err != nil {
+	// 	log.Fatalf("Opening sql database: %s", err.Error())
+	// }
+	cfg = parseConfig()
+	db, err = sql.Open("sqlite3", cfg.databasePath)
+	if err != nil {
+		log.Fatalf("Opening sql database: %s", err.Error())
 	}
-	return primitive.Transaction{
-		OrderId:           "order-id",
-		TransactionAmount: 50000,
-		PaymentType:       primitive.PaymentTypeEMoneyGopay,
-		TransactionStatus: primitive.TransactionStatusPending,
-		TransactionTime:   time.Now(),
-		ExpiresAt:         time.Now().Add(5 * time.Minute),
-	}, nil
-}
 
-type MockWebhookClient struct{}
+	db.SetMaxOpenConns(1)
 
-func (m *MockWebhookClient) Send(ctx context.Context, payload []byte) error {
-	return nil
-}
+	setupCtx, setupCancel := context.WithTimeout(context.Background(), time.Minute)
+	defer setupCancel()
 
-type MockEMoneyRepository struct{}
-
-func (m *MockEMoneyRepository) Migrate(ctx context.Context) error {
-	return nil
-}
-func (m *MockEMoneyRepository) CreateCharge(ctx context.Context, orderId string, amount int64, expiresAt time.Time) (string, error) {
-	return "emoney-id", nil
-}
-func (m *MockEMoneyRepository) GetByID(ctx context.Context, id string) (repository.Entry, error) {
-	if id != "emoney-id" {
-		return repository.Entry{}, repository.ErrNotFound
+	transactionRepository, err := transaction.NewTransactionRepository(db)
+	if err != nil {
+		log.Fatalf("Creating transaction repository: %s", err.Error())
 	}
-	return repository.Entry{
-		VirtualAccountNumber: "1234567890",
-		EMoneyID:             "emoney-id",
-		OrderId:              "order-id",
-		ChargedAmount:        50000,
-		ExpiresAt:            time.Now().Add(5 * time.Minute),
-	}, nil
-}
-func (m *MockEMoneyRepository) GetByOrderId(ctx context.Context, orderId string) (repository.Entry, error) {
-	if orderId != "order-id" {
-		return repository.Entry{}, repository.ErrNotFound
+	err = transactionRepository.Migrate(setupCtx)
+	if err != nil {
+		log.Fatalf("migrating transaction repository: %s", err.Error())
 	}
-	return repository.Entry{
-		VirtualAccountNumber: "1234567890",
-		EMoneyID:             "emoney-id",
-		OrderId:              "order-id",
-		ChargedAmount:        50000,
-		ExpiresAt:            time.Now().Add(5 * time.Minute),
-	}, nil
-}
-func (m *MockEMoneyRepository) CancelCharge(ctx context.Context, orderId string) error {
-	return nil
-}
-func (m *MockEMoneyRepository) DeductCharge(ctx context.Context, orderId string) error {
-	return nil
+
+	virtualAccountRepository, err := virtual_account.NewVirtualAccountRepository(db)
+	if err != nil {
+		log.Fatalf("Creating virtual account repository: %s", err.Error())
+	}
+	err = virtualAccountRepository.Migrate(setupCtx)
+	if err != nil {
+		log.Fatalf("migrating virtual account repository: %s", err.Error())
+	}
+
+	emoneyRepository, err := emoney.NewEmoneyRepository(db)
+	if err != nil {
+		log.Fatalf("Creating emoney repository: %s", err.Error())
+	}
+	err = emoneyRepository.Migrate(setupCtx)
+	if err != nil {
+		log.Fatalf("migrating emoney repository: %s", err.Error())
+	}
+
+	exitCode := m.Run()
+
+	err = db.Close()
+	if err != nil {
+		log.Printf("Closing database: %s", err.Error())
+	}
+
+	os.Exit(exitCode)
 }
 
-type MockVirtualAccountRepository struct{}
+type config struct {
+	httpHostname     string
+	httpPort         string
+	databasePath     string
+	webhookTargetURL string
+	serverKey        string
+}
 
-func (m *MockVirtualAccountRepository) Migrate(ctx context.Context) error {
-	return nil
-}
-func (m *MockVirtualAccountRepository) CreateOrGetVirtualAccountNumber(ctx context.Context, customerUniqueField string) (string, error) {
-	return "1234567890", nil
-}
-func (m *MockVirtualAccountRepository) CreateCharge(ctx context.Context, virtualAccountNumber string, orderId string, amount int64, expiresAt time.Time) (string, error) {
-	return "1234567890", nil
-}
-func (m *MockVirtualAccountRepository) GetByVirtualAccountNumber(ctx context.Context, virtualAccountNumber string) (repository.Entry, error) {
-	if virtualAccountNumber == "" {
-		return repository.Entry{}, fmt.Errorf("virtualAccountNumber is empty")
+func defaultConfig() config {
+	return config{
+		httpHostname: "localhost",
+		httpPort:     "3000",
+		databasePath: "payment.db",
 	}
-	if virtualAccountNumber == "empty-order-id" {
-		return repository.Entry{
-			VirtualAccountNumber: "1234567890",
-			EMoneyID:             "emoney-id",
-			OrderId:              "",
-			ChargedAmount:        50000,
-			ExpiresAt:            time.Now().Add(5 * time.Minute),
-		}, nil
-	}
-	if virtualAccountNumber != "order-id" {
-		return repository.Entry{}, repository.ErrNotFound
-	}
-	return repository.Entry{
-		VirtualAccountNumber: "1234567890",
-		EMoneyID:             "emoney-id",
-		OrderId:              "order-id",
-		ChargedAmount:        50000,
-		ExpiresAt:            time.Now().Add(5 * time.Minute),
-	}, nil
 }
-func (m *MockVirtualAccountRepository) GetByOrderId(ctx context.Context, orderId string) (repository.Entry, error) {
-	if orderId == "" {
-		return repository.Entry{}, repository.ErrNotFound
+
+func parseConfig() config {
+	result := defaultConfig()
+
+	if v, ok := os.LookupEnv("HTTP_HOSTNAME"); ok {
+		result.httpHostname = v
 	}
-	if orderId != "order-id" {
-		return repository.Entry{}, repository.ErrNotFound
+
+	if v, ok := os.LookupEnv("HTTP_PORT"); ok {
+		result.httpPort = v
 	}
-	return repository.Entry{
-		VirtualAccountNumber: "1234567890",
-		EMoneyID:             "emoney-id",
-		OrderId:              "order-id",
-		ChargedAmount:        50000,
-		ExpiresAt:            time.Now().Add(5 * time.Minute),
-	}, nil
-}
-func (m *MockVirtualAccountRepository) GetChargedAmount(ctx context.Context, virtualAccountNumber string) (int64, error) {
-	return 50000, nil
-}
-func (m *MockVirtualAccountRepository) DeductCharge(ctx context.Context, virtualAccountNumber string) error {
-	return nil
+
+	if v, ok := os.LookupEnv("DATABASE_PATH"); ok {
+		result.databasePath = v
+	}
+
+	if v, ok := os.LookupEnv("WEBHOOK_TARGET_URL"); ok {
+		result.webhookTargetURL = v
+	}
+
+	if v, ok := os.LookupEnv("SERVER_KEY"); ok {
+		result.serverKey = v
+	}
+
+	return result
 }
 
 func TestBusinessGetDetails(t *testing.T) {
-	// Create a mock of the repository
-	mockTransactionRepository := &MockTransactionRepository{}
-	mockWebhookClient := &MockWebhookClient{}
-	mockEMoneyRepository := &MockEMoneyRepository{}
-	mockVirtualAccountRepository := &MockVirtualAccountRepository{}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
-	conf := payment_service.Config{
-		ServerKey:                "server-key",
-		TransactionRepository:    mockTransactionRepository,
-		WebhookClient:            mockWebhookClient,
-		EMoneyRepository:         mockEMoneyRepository,
-		VirtualAccountRepository: mockVirtualAccountRepository,
+
+	transactionRepository, err := transaction.NewTransactionRepository(db)
+	if err != nil {
+		t.Errorf("creating transaction repository: %s", err.Error())
+	}
+	virtualAccountRepository, err := virtual_account.NewVirtualAccountRepository(db)
+	if err != nil {
+		t.Errorf("creating virtual account repository: %s", err.Error())
 	}
 
-	deps, err := payment_service.NewPaymentService(conf)
+	emoneyRepository, err := emoney.NewEmoneyRepository(db)
 	if err != nil {
-		t.Fatalf("creating payment service: %s", err.Error())
+		t.Errorf("creating emoney repository: %s", err.Error())
 	}
+	webhookClient, err := webhook.NewWebhookClient(cfg.webhookTargetURL)
+	if err != nil {
+		t.Errorf("creating webhook client: %s", err.Error())
+	}
+
+	paymentService, err := payment_service.NewPaymentService(payment_service.Config{
+		ServerKey:                cfg.serverKey,
+		TransactionRepository:    transactionRepository,
+		EMoneyRepository:         emoneyRepository,
+		WebhookClient:            webhookClient,
+		VirtualAccountRepository: virtualAccountRepository,
+	})
+	if err != nil {
+		t.Errorf("error: %s", err.Error())
+	}
+
+	t.Run("GetDetails should return not found if the order id is empty", func(t *testing.T) {
+		_, err = paymentService.GetDetail(ctx, "")
+		if err == nil {
+			t.Errorf("expecting error to be not nil, but got nil")
+		}
+	})
+	t.Run("GetDetails should return err 'not found' if the order id is not found", func(t *testing.T) {
+		_, err = paymentService.GetDetail(ctx, "not-exist")
+		if err == nil {
+			t.Errorf("expecting error to be not nil, but got nil")
+		}
+		if !errors.Is(err, business.ErrTransactionNotFound) {
+			t.Errorf("expecting error %s, instead got %v", business.ErrTransactionNotFound, err)
+		}
+	})
 
 	t.Run("GetDetails should return the correct details", func(t *testing.T) {
-		userId, err := deps.GetDetail(ctx, "order-id")
+		vaNumber, err := virtualAccountRepository.CreateOrGetVirtualAccountNumber(ctx, "annedoe@example.com")
 		if err != nil {
-			t.Fatalf("error: %s", err.Error())
+			delete(ctx, db, "virtual_accounts")
+			t.Errorf("unexpected error: %s", err.Error())
 		}
-		t.Logf("result: %v", userId)
+
+		orderId := "order-id"
+		_, err = virtualAccountRepository.CreateCharge(ctx, vaNumber, orderId, 50000, time.Now().Add(time.Hour))
+		if err != nil {
+			delete(ctx, db, "virtual_accounts")
+			delete(ctx, db, "virtual_account_entries")
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		_, err = emoneyRepository.CreateCharge(ctx, orderId, 50000, time.Now().Add(time.Hour))
+		if err != nil {
+			delete(ctx, db, "virtual_accounts")
+			delete(ctx, db, "virtual_account_entries")
+			delete(ctx, db, "emoney_entries")
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		_, err = paymentService.GetDetail(ctx, vaNumber)
+		if err == nil {
+			delete(ctx, db, "transaction_log")
+			delete(ctx, db, "virtual_accounts")
+			delete(ctx, db, "virtual_account_entries")
+			delete(ctx, db, "emoney_entries")
+			t.Errorf("expecting error to be not nil, but got nil")
+		}
+
+		err = transactionRepository.Create(ctx, repository.CreateTransactionParam{
+			OrderID:     orderId,
+			Amount:      50000,
+			PaymentType: primitive.PaymentTypeEMoneyQRIS,
+			Status:      primitive.TransactionStatusPending,
+			ExpiredAt:   time.Now().Add(time.Hour),
+		})
+		if err != nil {
+			if !errors.Is(err, repository.ErrDuplicate) {
+				delete(ctx, db, "transaction_log")
+				delete(ctx, db, "virtual_account_entries")
+				delete(ctx, db, "virtual_accounts")
+				delete(ctx, db, "emoney_entries")
+			}
+			t.Errorf("expecting not error when inserting transaction, instead got %s", err.Error())
+		}
+
+		_, err = paymentService.GetDetail(ctx, vaNumber)
+		if err != nil {
+			delete(ctx, db, "transaction_log")
+			delete(ctx, db, "virtual_accounts")
+			delete(ctx, db, "virtual_account_entries")
+			delete(ctx, db, "emoney_entries")
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		delete(ctx, db, "transaction_log")
+		delete(ctx, db, "virtual_account_entries")
+		delete(ctx, db, "virtual_accounts")
+		delete(ctx, db, "emoney_entries")
+
 	})
 
-	t.Run("GetDetails should return error if the order id is not found", func(t *testing.T) {
-		res, err := deps.GetDetail(ctx, "not-exist")
-		if err == nil {
-			t.Fatalf("error: %s", err.Error())
-		}
-		t.Logf("result: %v", res)
-	})
+}
 
-	t.Run("GetDetails should return error if the order id is empty", func(t *testing.T) {
-		res, err := deps.GetDetail(ctx, "")
-		if err == nil {
-			t.Fatalf("error: %s", err.Error())
-		}
-		t.Logf("result: %v", res)
-	})
-	t.Run("GetDetails should return error if return orderId of the virtual account is empty", func(t *testing.T) {
-		res, err := deps.GetDetail(ctx, "empty-order-id")
-		if err == nil {
-			t.Fatalf("error: %s", err.Error())
-		}
-		t.Logf("result: %v", res)
-	})
-
+func delete(ctx context.Context, db *sql.DB, table string) {
+	_, err := db.ExecContext(ctx, "DELETE FROM "+table)
+	if err != nil {
+		log.Fatalf("deleting %s: %s", table, err.Error())
+	}
 }
